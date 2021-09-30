36f851d9aef0800d786bd25e4b06bdf48e9fb9b6
Debora <dehfachini@gmail.com>
2021-09-29 21:23:17 -0300

    remove error message on input

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const button = document.querySelector(".icon-arrow")
+const emailInput = document.getElementById("email")
+const borderInitialColor = emailInput.style.borderColor;
+const errorMessageEl = document.getElementById("error-message")
 
 button.addEventListener("click", function () {
-    const emailInput = document.getElementById("email")
     const value = emailInput.value
-    const errorMessageEl = document.getElementById("error-message")
 
     if (value === "") {
-        errorMessageEl.style.display="block"
-        emailInput.style.borderColor="red"
+        errorMessageEl.style.display = "block"
+        emailInput.style.borderColor = "red"
     } else if (!value.includes("@") || !value.includes(".")) {
-        errorMessageEl.style.display="block"
-        emailInput.style.borderColor="red"
+        errorMessageEl.style.display = "block"
+        emailInput.style.borderColor = "red"
     }
 
 })
 
+
+emailInput.addEventListener("input", function () {
+    emailInput.style.borderColor = borderInitialColor;
+    errorMessageEl.style.display = "none";
+})
 
