fc0a24cc394be77f01525ef380a8b460042f67c2
Debora <dehfachini@gmail.com>
2021-09-29 21:43:37 -0300

    Simplify code

--- app.js
+++ app.js
@@ -4,21 +4,17 @@ const borderInitialColor = emailInput.style.borderColor;
 const errorMessageEl = document.getElementById("error-message")
 
 button.addEventListener("click", function () {
-   validateEmail()
+    validateEmail()
 })
 
 emailInput.addEventListener("input", function () {
-   removeErrorMsg()
+    removeErrorMsg()
 })
-
 
 function validateEmail() {
     const value = emailInput.value
 
-    if (value === "") {
-        errorMessageEl.style.display = "block"
-        emailInput.style.borderColor = "red"
-    } else if (!value.includes("@") || !value.includes(".")) {
+    if (!value || !value.includes("@") || !value.includes(".")) {
         errorMessageEl.style.display = "block"
         emailInput.style.borderColor = "red"
     }
